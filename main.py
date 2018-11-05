00bde15387905415beb7f3e46ba60d7640f2a80b
Grzegorz <grzegorz.wilczynski@student.put.poznan.pl>
2018-11-05 02:12:43 +0100

    Działa długo

--- main.py
+++ main.py
@@ -6,39 +6,111 @@ import datetime
 import sqlite3
 
 
-class MyDate:
-    id: int
-    year: int
-    month: int
-    day: int
+class PlayInserter:
+    conn: sqlite3.Connection
 
-    def __init__(self, id: int):
-        date = datetime.datetime.utcfromtimestamp(id)
-        self.year = date.year
-        self.month = date.month
-        self.day = date.day
+    def __init__(self, conn: sqlite3.Connection):
+        self.conn = conn
+        self.create_table()
+
+    def create_table(self):
+        c = self.conn.cursor()
+        c.execute("""create table plays (
+                    id integer primary key,
+                    date_id integer,
+                    user_id integer,
+                    song_id integer,
+                    FOREIGN KEY (date_id) REFERENCES dates(id),
+                    FOREIGN KEY (user_id) REFERENCES users(id),
+                    FOREIGN KEY (song_id) REFERENCES songs(id));
+                  """)
+        c.close()
+
+    def insert_play_to_db(self, date_id: int, user_id: int, song_id: int):
+        c = self.conn.cursor()
+        c.execute("""insert into plays (date_id, user_id, song_id) values (?, ?, ?);""", [date_id, user_id, song_id])
+        c.close()
+
+
+class DateGetter:
+    conn: sqlite3.Connection
+
+    def __init__(self, conn: sqlite3.Connection):
+        self.conn = conn
+        self.create_table()
+
+    def create_table(self):
+        c = self.conn.cursor()
+        c.execute("""create table dates (
+                    id integer primary key,
+                    day integer,
+                    month integer,
+                    year integer);
+                  """)
+        c.execute("""create index dates_day_idx on dates(day);""")
+        c.execute("""create index dates_month_idx on dates(month);""")
+        c.execute("""create index dates_year_idx on dates(year);""")
+        c.close()
+
+    def get_date_id(self, unix_timestamp: int):
+        date = datetime.datetime.utcfromtimestamp(unix_timestamp)
+        date_id = self.get_date_id_from_db(date.day, date.month, date.year)
+        if not date_id:
+            self.insert_date_to_db(date.day, date.month, date.year)
+            date_id = self.get_date_id_from_db(date.day, date.month, date.year)
+
+        return date_id
+
+    def get_date_id_from_db(self, day: int, month: int, year: int):
+        c = self.conn.cursor()
+        c.execute("""select id from dates where day = ? and month = ? and year = ?;""", [day, month, year])
+        result = c.fetchall()
+        c.close()
+        if len(result) > 0:
+            return result[0][0]
+
+    def insert_date_to_db(self, day: int, month: int, year: int):
+        c = self.conn.cursor()
+        c.execute("""insert into dates (day, month, year) values (?, ?, ?);""", [day, month, year])
+        c.close()
 
 
 class UserGetter:
-    users: dict
-    next_id: int
+    conn: sqlite3.Connection
 
-    def __init__(self):
-        self.users = dict()
-        self.next_id = 0
+    def __init__(self, conn: sqlite3.Connection):
+        self.conn = conn
+        self.create_table()
 
-    def get_user(self, name: str):
-        try:
-            user_to_return = self.users[name]
-        except KeyError:
-            user_to_return = None
+    def create_table(self):
+        c = self.conn.cursor()
+        c.execute("""create table users (
+                    id integer primary key,
+                    user_id TEXT);
+                  """)
+        c.execute("""create unique index users_id_idx on users(user_id);""")
+        c.close()
 
-        if not user_to_return:
-            user_to_return = User(self.next_id, name)
-            self.next_id += 1
-            self.users[name] = user_to_return
+    def get_user_id(self, id: str):
+        user_id = self.get_user_id_from_db(id)
+        if not user_id:
+            self.insert_user_to_db(id)
+            user_id = self.get_user_id_from_db(id)
 
-        return user_to_return
+        return user_id
+
+    def get_user_id_from_db(self, id: str):
+        c = self.conn.cursor()
+        c.execute("""select * from users where user_id = ?;""", [id])
+        result = c.fetchall()
+        c.close()
+        if len(result) > 0:
+            return result[0][0]
+
+    def insert_user_to_db(self, id: str):
+        c = self.conn.cursor()
+        c.execute("""insert into users (user_id) values (?);""", [id])
+        c.close()
 
 
 class User:
@@ -91,9 +163,11 @@ class ArtistGetter:
 class SongGetter:
     conn: sqlite3.Connection
 
-    def __init__(self, conn: sqlite3.Connection):
+    def __init__(self, conn: sqlite3.Connection, create_table: bool):
         self.conn = conn
-        self.create_table()
+
+        if create_table:
+            self.create_table()
 
     def create_table(self):
         c = self.conn.cursor()
@@ -110,7 +184,7 @@ class SongGetter:
 
     def get_id(self, song_id: str):
         c = self.conn.cursor()
-        c.execute("""select (id, track_id, song_id, artist_id, title) from songs where song_id = ?;""", [song_id])
+        c.execute("""select id from songs where song_id = ?;""", [song_id])
         result = c.fetchall()
         c.close()
         if len(result) > 0:
@@ -123,35 +197,36 @@ class SongGetter:
         c.close()
 
 
-def process_unique_tracks(file_path: str):
-    conn = sqlite3.connect(":memory:")
+def process_unique_tracks(file_path: str, conn: sqlite3.Connection):
+
     artist_getter = ArtistGetter(conn)
-    songs_getter = SongGetter(conn)
+    songs_getter = SongGetter(conn, True)
 
     with open(file_path, "r", encoding='utf-8', errors='ignore') as myfile:
         line = myfile.readline()
         while line:
             split_line = line[0:-1].split("<SEP>")
-            songs_getter.insert_song_to_db(split_line[0], split_line[1], artist_getter.get_artist_id(split_line[2]), split_line[3])
+            songs_getter.insert_song_to_db(split_line[0], split_line[1], artist_getter.get_artist_id(split_line[2]),
+                                           split_line[3])
             line = myfile.readline()
 
-    return artist_getter, songs_getter
 
-'''
-def process_triplets(file_path: str, song_getter: SongGetter):
-    listenings: List[Listening] = []
-    user_getter = UserGetter()
+def process_triplets(file_path: str, conn: sqlite3.Connection):
+    user_getter = UserGetter(conn)
+    dates_getter = DateGetter(conn)
+    song_getter = SongGetter(conn, False)
+    play_inserter = PlayInserter(conn)
 
     with open(file_path, "r", encoding='utf-8', errors='ignore') as myfile:
         line = myfile.readline()
         while line:
             split_line = line[0:-1].split("<SEP>")
-            listenings.append(Listening(MyDate(int(split_line[2])), user_getter.get_user(split_line[0]),
-                              song_getter.get(split_line[1])))
+            user_id = user_getter.get_user_id(split_line[0])
+            date_id = dates_getter.get_date_id(int(split_line[2]))
+            song_id = song_getter.get_id(split_line[1])
+            play_inserter.insert_play_to_db(date_id, user_id, song_id)
             line = myfile.readline()
 
-    return listenings
-'''
 
 def main():
     conn = sqlite3.connect(":memory:")
@@ -161,11 +236,12 @@ def main():
 
     start_time = time.time()
     print("start: process_unique_tracks")
-    artist_getter, songs_getter = process_unique_tracks(unique_tracks_path)
+    process_unique_tracks(unique_tracks_path, conn)
     elapsed_time = time.time() - start_time
     print(elapsed_time)
+
     print("start: process_triplets")
-    #listenings = process_triplets(triplets_sample_path, songs_getter)
+    process_triplets(triplets_sample_path, conn)
     elapsed_time = time.time() - start_time
     print(elapsed_time)
 
